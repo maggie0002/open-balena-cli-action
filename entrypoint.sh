c4b5841ec0786775c20537910816f2e6e06e0b14
Dmytro Teslov <hellios.dt@gmail.com>
2021-03-12 09:47:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,10 +26,7 @@ if [[ "${INPUT_ROOT_CERT}" != "" ]]; then
   cp ca.crt /usr/local/share/ca-certificates/ca.crt
   update-ca-certificates
 fi
-echo ${PWD}
-
-npm config set strict-ssl false --global
-alias balena='NODE_EXTRA_CA_CERTS=/usr/local/share/ca-certificates/ca.crt /usr/bin/balena'
+export "NODE_EXTRA_CA_CERTS=/usr/local/share/ca-certificates/ca.crt"
 # Log in to Balena
 balena login --token ${INPUT_BALENA_API_TOKEN} > /dev/null
 balena ${INPUT_BALENA_COMMAND}
