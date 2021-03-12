40f04cfae9b73aa73960193896e0597ef9934516
Dmytro Teslov <hellios.dt@gmail.com>
2021-03-12 10:19:19 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,8 @@ if [[ "${INPUT_ROOT_CERT}" != "" ]]; then
   chmod 644 ca.crt
   update-ca-certificates
 fi
-
+echo ${INPUT_ROOT_CERT}
+echo ${INPUTE_BALENA_COMMAND}
 export "NODE_EXTRA_CA_CERTS=/usr/local/share/ca-certificates/ca.crt"
 # Log in to Balena
 balena login --token ${INPUT_BALENA_API_TOKEN} > /dev/null
