10085da09a7a20046bd60429f4573254b79a5f06
Dmytro Teslov <hellios.dt@gmail.com>
2021-03-12 09:58:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,9 +24,9 @@ fi
 if [[ "${INPUT_ROOT_CERT}" != "" ]]; then
   echo ${INPUT_ROOT_CERT} > ca.crt
   cp ca.crt /usr/local/share/ca-certificates/ca.crt
-  update-ca-certificates
   chmod 644 /usr/local/share/ca-certificates/ca.crt
   chmod 644 ca.crt
+  update-ca-certificates
 fi
 
 export "NODE_EXTRA_CA_CERTS=/usr/local/share/ca-certificates/ca.crt"
